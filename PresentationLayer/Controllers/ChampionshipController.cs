e6db38e0fe76521d9e3363c6172b5ae8db3a9917
mostafa hamed <mos.hamed11@hotmail.com>
2018-05-15 16:09:26 +0200

    add championship controller

---
+++ PresentationLayer/Controllers/ChampionshipController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PresentationLayer.Models;
+using PresentationLayer.Models.dbModels;
+
+namespace PresentationLayer.Controllers
+{
+    public class ChampionshipController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Championships
+        public ActionResult Index()
+        {
+            return View(db.Championships.ToList());
+        }
+
+        // GET: Championships/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Championship Championship = db.Championships.Find(id);
+            if (Championship == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Championship);
+        }
+
+        // GET: Championships/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Championships/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstTeamName,SecondTeamName,FirstTeamGoals,SecondTeamGoals,ChampionshipDateTime,IsPlayed,GUID,CreatedTime,CreatedBy")] Championship Championship)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Championships.Add(Championship);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Championship);
+        }
+
+        // GET: Championships/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Championship Championship = db.Championships.Find(id);
+            if (Championship == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Championship);
+        }
+
+        // POST: Championships/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstTeamName,SecondTeamName,FirstTeamGoals,SecondTeamGoals,ChampionshipDateTime,IsPlayed,GUID,CreatedTime,CreatedBy")] Championship Championship)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Championship).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Championship);
+        }
+
+        // GET: Championships/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Championship Championship = db.Championships.Find(id);
+            if (Championship == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Championship);
+        }
+
+        // POST: Championships/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Championship Championship = db.Championships.Find(id);
+            db.Championships.Remove(Championship);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- PresentationLayer/Models/IdentityModels.cs
+++ PresentationLayer/Models/IdentityModels.cs
@@ -22,7 +22,7 @@ namespace PresentationLayer.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("SportsContext", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
